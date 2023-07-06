5feb41fc4b4d8696047e3f87ddf367430f45345a
kreyno93 <michael.wagener@neuefische.de>
2023-07-06 14:41:16 +0200

    update.main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "aws" {
 
 resource "aws_instance" "deham6demos"{
     ami = "ami-0fa167c2af1ea0840"
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
     key_name = "vockey"
     vpc_security_group_ids = ["sg-07053caff48096429"]
     subnet_id = "subnet-07e16952d8589584f"
