71b962afdcf090d454d5af7f25c472ca270b2e98
kreyno93 <michael.wagener@neuefische.de>
2023-07-06 13:20:20 +0200

    update mian.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "deham6demos"{
     instance_type = "t2.micro"
     key_name = "deham630062023"
     vpc_security_group_ids = ["sg-0668745a1e96b9e55"]
-    subnet_id = "subnet-05527afe89ba4d0ee"
+    subnet_id = "subnet-0b4e690242ed46166"
     tags = {
         Name = "deham6demos"
     }
